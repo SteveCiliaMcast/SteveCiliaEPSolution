d226fac756e3296b4fe2dbbc6d36e262465083f7
SteveCiliaMcast <steve.cilia.f32203@mcast.edu.mt>
2025-04-03 22:53:24 +0200

    Add project files.

---
+++ SteveCiliaEPSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SteveCiliaEPSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
